fe2c9be843c1fc437bb17b94545d3e99926b6c74
kzh19891208 <68722354+kzh19891208@users.noreply.github.com>
2022-12-17 14:52:48 +0800

    update home page

--- src/Page/Home/HomePage.js
+++ src/Page/Home/HomePage.js
@@ -22,6 +22,36 @@ class HomePage extends Component {
                     </div>
                 </div>
             </div>
+            <div className="HomePageContent">
+                <span>No-to-AI NFT</span> is the contract of ERC721A, with a total of 2020 pieces issued. You will see
+                different <span>No-to-AI NFTs</span>. This is not only a declaration in the field of traditional art,
+                but also in the field of web3, we should respect the creativity from our human brains. That's the soul.
+            </div>
+            <div style={{
+                height: "2px",
+                width: "100%",
+                marginTop: "10px",
+                marginBottom: "8px",
+                background: "#ffffff"
+            }}/>
+            <div className="HomePageContent">
+                <span>No-to-AI</span> can be seen as a light WEB3 digital art fund. We will put the NFT revenue in the
+                treasury and reward some artists who contribute their works every week. We will divide the artists'
+                works into ERC1155 NFT equal to <span>No-to-AI NFT</span>, and share the rights and interests to every
+                holder.
+                Royalty also
+                enters the treasury as a reward for artists.
+            </div>
+            <div
+                style={{
+                    marginTop: "29px",
+                    marginBottom:"150px"
+                }}
+                className="HomePageContent">
+                All holders of <span>No-to-AI NFT</span> will receive airdrops and free-mints of digital artworks constantly from
+                outstanding artists around the world. At the same time, we also encourage like-minded friends in the
+                community to share their creations and collect them together.
+            </div>
         </div>);
     }
 }
