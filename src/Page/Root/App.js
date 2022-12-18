ac680deda72afb07e73a17705549640a16dfaa59
kzh19891208 <68722354+kzh19891208@users.noreply.github.com>
2022-12-18 09:37:20 +0800

    update web page

--- src/Page/Root/App.js
+++ src/Page/Root/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import HomePage from "../Home/HomePage";
 import Artwork from "../Artwork/Artwork";
 
 function App() {
+
+    const [selectID, setSelectID] = useState([])
+
+    useEffect(() => {
+        setSelectID(0);
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     const OnMintClick = () => {
         console.log('mint click\r\n');
@@ -12,18 +18,55 @@ function App() {
         console.log('mint connect wallet\r\n');
     };
 
+    const BuildContent = (id) => {
+        if (id === 0) {
+            return (
+                <HomePage
+                    OnMintClicked={OnMintClick}
+                    OnConnectClicked={OnConnectClick}
+                    Address={undefined}
+                    SoldOut={false}
+                    MintMax={2020}
+                    MintCurrent={0}
+                    MintPrise={0.02}
+                />
+            );
+        } else if (id === 1) {
+            return (
+                <Artwork/>
+            );
+        }
+    }
+
     return (
         <div className="App">
-            <HomePage
-                OnMintClicked={OnMintClick}
-                OnConnectClicked={OnConnectClick}
-                Address={undefined}
-                SoldOut={false}
-                MintMax={2020}
-                MintCurrent={0}
-                MintPrise={0.02}
-            />
-            <Artwork/>
+            <div className='AppHeader'>
+                <img
+                    className='AppHeaderLogo'
+                    src={process.env.PUBLIC_URL + '/images/Logo.png'}
+                    alt='banner'/>
+                <div
+                    style={{
+                        opacity: (selectID === 0) ? 1 : 0.5
+                    }}
+                    onClick={() => {
+                        setSelectID(0)
+                    }}
+                    className='AppHeaderItem'>
+                    Mint<span>No-to-AI</span>NFT
+                </div>
+                <div
+                    style={{
+                        opacity: (selectID === 1) ? 1 : 0.5
+                    }}
+                    onClick={() => {
+                        setSelectID(1)
+                    }}
+                    className='AppHeaderItem'>
+                    Artwork airdrop
+                </div>
+            </div>
+            {BuildContent(selectID)}
         </div>
     );
 }
